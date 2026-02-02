35f9d25eb566beb8308711b11b9ddb22f8f5e1ac
netwjx <netwjx@gmail.com>
2026-02-02 16:08:55 +0800

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+  base: '/hermatch_svelte/',
   plugins: [svelte()],
 });
